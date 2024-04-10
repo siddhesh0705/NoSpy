7ae88ddcc45a8e606ec5a5f9170d878184fd5323
siddhesh <siddheshsangale0705@gmail.com>
2024-04-10 18:25:36 +0530

    socket io chat app

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
-const WebSocket = require('ws');
+const cors = require('cors');
+
 const dotenv = require('dotenv');
 
 
@@ -12,6 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
 
 
 const authRouter = require('../PBL authentication/routes/auth');
@@ -33,46 +35,33 @@ app.use(errorHandlerMiddleware);
 const server = http.createServer(app);
 
 
-const wss = new WebSocket.Server({ server });
-
+const io = require('socket.io')(server)
+  
 
 const Message = require('./model/message');
 
-wss.on('connection', ws => {
-  console.log('Client connected');
-
-  ws.on('message', message => {
-    console.log('Received:', message);
-
-    // Parse the incoming JSON message
-    try {
-      const data = JSON.parse(message);
-      console.log('Parsed JSON:', data);
-
-      // Extract sender, receiver, and text from the message
-      const { sender, receiver, text } = data;
-
-      // Create a new message document and save it to the database
-      const newMessage = new Message({
-        sender,
-        receiver,
-        text
-      });
-
-      newMessage.save()
-        .then(() => {
-          console.log('Message saved to database');
-        })
-        .catch(error => {
-          console.error('Error saving message to database:', error);
-        });
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-    }
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
   });
+  socket.on("message", async (msg) => {
+    console.log(msg);
+    try {
+      const message = new Message(msg);
+      await message.save();
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
+      console.log("message saved in mongo:".message);
+      let targetId = msg.targetId;
+      if (clients[targetId]) clients[targetId].emit("message", msg);
+      
+    } catch (error) {
+      console.log("error occured while sending message",error);
+    }
+    
   });
 });
 
@@ -93,14 +82,14 @@ const connectDB = async () => {
     console.error('Database connection error:', error);
   }
 };
-const host = '192.168.6.33'; 
+//const host = '192.168.6.33'; 
 const start = async () => {
   try {
     await connectDB();
     const port = process.env.PORT || 5000;
 
-    server.listen(port, host, () => {
-      console.log(`Server is listening on http://${host}:${port}`); 
+    server.listen(port,"0.0.0.0", () => {
+      console.log(`Server is listening on http://${port}`); 
     });
 
   } catch (error) {
